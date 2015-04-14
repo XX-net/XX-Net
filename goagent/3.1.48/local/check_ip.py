f159938dd6282aa7b8e1fd25aa766c098bf0eae7
Michael.X <xxnet.dev@gmail.com>
2015-04-14 03:28:38 -0400

    add test keep alive

--- goagent/3.1.48/local/check_ip.py
+++ goagent/3.1.48/local/check_ip.py
@@ -83,54 +83,55 @@ class Check_frame(object):
         self.result = Check_result()
         self.ip = ip
 
-    def check(self, callback=None, check_ca=False):
+        self.timeout = 5
+        self.openssl_context = SSLConnection.context_builder(ssl_version="TLSv1", ca_certs=g_cacertfile) # check cacert cost too many cpu, 100 check thread cost 60%.
 
-        timeout = 5
-        openssl_context = SSLConnection.context_builder(ssl_version="TLSv1", ca_certs=g_cacertfile) # check cacert cost too many cpu, 100 check thread cost 60%.
+    def connect_ssl(self, ip):
+        import struct
+        ip_port = (ip, 443)
+
+        if config.PROXY_ENABLE:
+            sock = socks.socksocket(socket.AF_INET)
+        else:
+            sock = socket.socket(socket.AF_INET)
+        # set reuseaddr option to avoid 10048 socket error
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # set struct linger{l_onoff=1,l_linger=0} to avoid 10048 socket error
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+        # resize socket recv buffer 8K->32K to improve browser releated application performance
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 32*1024)
+        # disable negal algorithm to send http request quickly.
+        sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, True)
+        # set a short timeout to trigger timeout retry more quickly.
+        sock.settimeout(self.timeout)
+
+        ssl_sock = SSLConnection(self.openssl_context, sock)
+        ssl_sock.set_connect_state()
+
+        # pick up the certificate
+        #server_hostname = random_hostname() if (cache_key or '').startswith('google_') or hostname.endswith('.appspot.com') else None
+        #if server_hostname and hasattr(ssl_sock, 'set_tlsext_host_name'):
+        #    ssl_sock.set_tlsext_host_name(server_hostname)
+
+        time_begin = time.time()
+        ssl_sock.connect(ip_port)
+        time_connected = time.time()
+        ssl_sock.do_handshake()
+        time_handshaked = time.time()
+
+        self.result.connct_time = int((time_connected - time_begin) * 1000)
+        self.result.handshake_time = int((time_handshaked - time_connected) * 1000)
+        logging.debug("conn: %d  handshake:%d", self.result.connct_time, self.result.handshake_time)
+
+        # sometimes, we want to use raw tcp socket directly(select/epoll), so setattr it to ssl socket.
+        ssl_sock.sock = sock
+        return ssl_sock
+
+    def check(self, callback=None, check_ca=False, close_ssl=True):
 
         ssl_sock = None
         try:
-            def connect_ssl(ip):
-                import struct
-                ip_port = (ip, 443)
-
-                if config.PROXY_ENABLE:
-                    sock = socks.socksocket(socket.AF_INET)
-                else:
-                    sock = socket.socket(socket.AF_INET)
-                # set reuseaddr option to avoid 10048 socket error
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                # set struct linger{l_onoff=1,l_linger=0} to avoid 10048 socket error
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
-                # resize socket recv buffer 8K->32K to improve browser releated application performance
-                sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 32*1024)
-                # disable negal algorithm to send http request quickly.
-                sock.setsockopt(socket.SOL_TCP, socket.TCP_NODELAY, True)
-                # set a short timeout to trigger timeout retry more quickly.
-                sock.settimeout(timeout)
-
-                ssl_sock = SSLConnection(openssl_context, sock)
-                ssl_sock.set_connect_state()
-
-                # pick up the certificate
-                #server_hostname = random_hostname() if (cache_key or '').startswith('google_') or hostname.endswith('.appspot.com') else None
-                #if server_hostname and hasattr(ssl_sock, 'set_tlsext_host_name'):
-                #    ssl_sock.set_tlsext_host_name(server_hostname)
-
-                time_begin = time.time()
-                ssl_sock.connect(ip_port)
-                time_connected = time.time()
-                ssl_sock.do_handshake()
-                time_handshaked = time.time()
-
-                self.result.connct_time = int((time_connected - time_begin) * 1000)
-                self.result.handshake_time = int((time_handshaked - time_connected) * 1000)
-                logging.debug("conn: %d  handshake:%d", self.result.connct_time, self.result.handshake_time)
-
-                # sometimes, we want to use raw tcp socket directly(select/epoll), so setattr it to ssl socket.
-                ssl_sock.sock = sock
-                return ssl_sock
-            ssl_sock = connect_ssl(self.ip)
+            ssl_sock = self.connect_ssl(self.ip)
 
             # verify SSL certificate issuer.
             def check_ssl_cert(ssl_sock):
@@ -157,7 +158,7 @@ class Check_frame(object):
         except SSLError as e:
             logging.debug("Check_appengine %s SSLError:%s", self.ip, e)
         except IOError as e:
-            logging.debug("Check %s IOError:%s", self.ip, e)
+            logging.warn("Check %s IOError:%s", self.ip, e)
             pass
         except httplib.BadStatusLine:
             #logging.debug('Check_appengine http.bad status line ip:%s', ip)
@@ -171,10 +172,21 @@ class Check_frame(object):
                 errno_str = e.message
             logging.debug('check_appengine %s %s err:%s', self.ip, errno_str, e)
         finally:
-            if ssl_sock:
+            if ssl_sock and close_ssl:
                 ssl_sock.close()
 
         return False
+
+def test_keepalive(ip_str, interval=5):
+    logging.info("==>%s", ip_str)
+    check = Check_frame(ip_str)
+    ssl = check.connect_ssl(ip_str)
+    result = test_app_head(ssl, ip_str)
+    print result
+    time.sleep(interval)
+    result = test_app_head(ssl, ip_str)
+    print result
+
 
 # each ssl connection must reuse by same host
 # therefor, different host need new ssl connection.
@@ -198,6 +210,29 @@ def test_server_type(ssl_sock, ip):
     finally:
         response.close()
 
+
+def test_app_head(ssl_sock, ip):
+    request_data = 'HEAD /_gh/ HTTP/1.1\r\nHost: xxnet-100.appspot.com\r\n\r\n'
+    time_start = time.time()
+    ssl_sock.send(request_data.encode())
+    response = httplib.HTTPResponse(ssl_sock, buffering=True)
+    try:
+        response.begin()
+        status = response.status
+        if status != 200:
+            logging.debug("app check %s status:%d", ip, status)
+            raise Exception("app check fail")
+        return True
+        content = response.read()
+        if not content == "CHECK_OK":
+            logging.debug("app check %s content:%s", ip, content)
+            #raise Exception("content fail")
+    finally:
+        response.close()
+    time_stop = time.time()
+    time_cost = (time_stop - time_start)*1000
+    logging.debug("app check time:%d", time_cost)
+    return True
 
 def test_app_check(ssl_sock, ip):
     request_data = 'GET /check HTTP/1.1\r\nHost: xxnet-check.appspot.com\r\n\r\n'
@@ -279,7 +314,7 @@ def test_with_app(ip_str):
     check = Check_frame(ip_str)
 
     result = check.check(callback=test_app_check, check_ca=True)
-    logging.info("test %s app %s", ip_str, result)
+    logging.info("test_with_app %s app %s", ip_str, result)
 
 
 
@@ -391,7 +426,8 @@ def check_all_exist_ip():
             handshake_time = int(str_l[3])
 
             logging.info("test ip: %s time:%d domain:%s server:%s", ip_str, handshake_time, domain, server)
-            test_with_app(ip_str)
+            #test_with_app(ip_str)
+            test_gws(ip_str)
             #self.add_ip(ip_str, handshake_time, domain, server)
         except Exception as e:
             logging.exception("load_ip line:%s err:%s", line, e)
@@ -403,10 +439,10 @@ if __name__ == "__main__":
     #print network_is_ok()
     #test("216.58.220.86", 10) #gws
     #test('208.117.224.213', 10)
-    test("127.0.0.1")
+    #test("127.0.0.1")
     #test_main()
-    #check_all_exist_ip()
-
+    check_all_exist_ip()
+    #test_gws("210.158.146.245")
 
 # about ip connect time and handshake time
 # handshake time is double of connect time in common case.
