0738156a50c1c28891804ef14e1bd9a21be2c539
Cat73 <1901803382@qq.com>
2015-05-02 22:29:23 +0800

    Goagent状态页提示使用tip函数 tip部分的代码挪到ui.js

---
+++ launcher/1.4.0/web_ui/js/ui.js
@@ -0,0 +1,26 @@
+function title(title) {
+    $('#title').text(title);
+}
+
+function tip(message, type, allowOff) {
+    $('#tip-message').html(message);
+    $('#tip').removeClass('alert-error');
+    $('#tip').removeClass('alert-success');
+    $('#tip').removeClass('hide');
+    $('#tip').addClass('alert-' + type);
+    if( allowOff === undefined || allowOff === true ) {
+        $('#tip-close').css('display', '');
+    } else {
+        $('#tip-close').css('display', 'none');
+    }
+}
+function tipHide() {
+    $('#tip').addClass('hide');
+}
+function tipHasHide() {
+    return $('#tip').hasClass('hide');
+}
+
+$('#tip-close').click(function() {
+    $('#tip').addClass('hide');
+});
