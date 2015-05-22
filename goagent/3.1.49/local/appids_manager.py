09b9b6d0b54d5890d2d1bfd72c79bd9d9d0287d2
Cat73 <1901803382@qq.com>
2015-05-22 15:18:35 +0800

    APPID显示异常的修复

--- goagent/3.1.49/local/appids_manager.py
+++ goagent/3.1.49/local/appids_manager.py
@@ -7,9 +7,7 @@ import random
 
 class APPID_manager(object):
     def __init__(self):
-        self.working_appid_list = config.GAE_APPIDS
-        self.out_of_quota_appids = []
-        self.not_exist_appids = []
+        self.reset_appid()
 
     def get_appid(self):
         if len(self.working_appid_list) == 0:
@@ -25,7 +23,7 @@ class APPID_manager(object):
         except:
             pass
         if len(self.working_appid_list) == 0:
-            self.working_appid_list = config.GAE_APPIDS
+            self.reset_appid()
 
     def report_not_exist(self, appid):
         #logging.error("APPID_manager, report_not_exist %s", appid)
@@ -42,7 +40,7 @@ class APPID_manager(object):
             pass
 
         if len(self.working_appid_list) == 0:
-            self.working_appid_list = config.GAE_APPIDS
+            self.reset_appid()
 
     def appid_exist(self, appids):
         for appid in appids.split('|'):
@@ -51,5 +49,10 @@ class APPID_manager(object):
             if appid in config.GAE_APPIDS:
                 return True
         return False
+
+    def reset_appid(self):
+        self.working_appid_list = config.GAE_APPIDS
+        self.out_of_quota_appids = []
+        self.not_exist_appids = []
 
 appid_manager = APPID_manager()
