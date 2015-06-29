648f7736c68a1489786f88c8282520a0ab3d40e2
YFdyh000 <yfdyh000@gmail.com>
2015-06-29 22:11:02 +0800

    Skip output sort time if tiny

--- goagent/3.1.49/local/google_ip.py
+++ goagent/3.1.49/local/google_ip.py
@@ -183,7 +183,8 @@ class Check_ip():
             self.ip_lock.release()
 
         time_cost = (( time.time() - self.last_sort_time_for_gws) * 1000)
-        logging.debug("sort ip time:%d", time_cost) # 5ms for 1000 ip.
+        if time_cost < 30: return
+        logging.debug("sort ip time:%dms", time_cost) # 5ms for 1000 ip. 70~150ms for 30000 ip.
 
     def get_gws_ip(self):
         self.try_sort_ip_by_handshake_time()
