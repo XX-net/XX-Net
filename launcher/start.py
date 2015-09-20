f04462b5d08691afb9b3124231212c3512d1b354
aOJzQT <aOJzQT@users.noreply.github.com>
2015-09-20 13:48:21 +0000

    Update start.py
    minor typo

--- launcher/start.py
+++ launcher/start.py
@@ -86,7 +86,7 @@ def main():
 
 
     if has_desktop and config.get(["modules", "launcher", "popup_webui"], 1) == 1:
-        host_port = config.config["modules"]["launcher"].get("control_port", 8085)
+        host_port = config.get(["modules", "launcher", "control_port"], 8085)
         webbrowser.open("http://127.0.0.1:%s/" % host_port)
 
     update.start()
