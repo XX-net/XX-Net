0df9f3dae55da82b55939af41761b71c1fe19440
Michael.X <xxnet.dev@gmail.com>
2015-10-02 19:29:00 +0800

    fix bug: remove good ip

--- gae_proxy/local/gae_handler.py
+++ gae_proxy/local/gae_handler.py
@@ -309,7 +309,7 @@ def handler(method, url, headers, body, wfile):
                 xlog.warn("fetch gae status:%s url:%s", response.app_status, url)
 
                 server_type = response.getheader('Server', "")
-                if "gws" not in server_type:
+                if "gws" not in server_type and "Google Frontend" not in server_type:
                     xlog.warn("IP:%s not support GAE, server type:%s", response.ssl_sock.ip, server_type)
                     google_ip.report_connect_fail(response.ssl_sock.ip, force_remove=True)
                     response.close()
@@ -407,14 +407,15 @@ def handler(method, url, headers, body, wfile):
             start, end, length = tuple(int(x) for x in re.search(r'bytes (\d+)-(\d+)/(\d+)', content_range).group(1, 2, 3))
         else:
             start, end, length = 0, content_length-1, content_length
-        body_length = end - start
+        body_length = end - start + 1
 
         last_read_time = time.time()
         while True:
             if start > end:
+                response.ssl_sock.received_size += body_length
+                google_ip.report_ip_traffic(response.ssl_sock.ip, body_length)
                 https_manager.save_ssl_connection_for_reuse(response.ssl_sock, call_time=time_request)
                 xlog.info("GAE %d|%s|%d t:%d s:%d %d %s", response.ssl_sock.fd, response.ssl_sock.ip, response.ssl_sock.received_size, (time.time()-time_request)*1000, length, response.status, url)
-                response.ssl_sock.received_size += body_length
                 return
 
             data = response.read(config.AUTORANGE_BUFSIZE)
