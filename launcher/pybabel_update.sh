4f0b89e3633ad4a5505862d4f943636165813b0a
Mike Chavez <mikedchavez1010@gmail.com>
2015-12-18 13:26:00 +0800

    Rename advance.html to advanced.html

--- launcher/pybabel_update.sh
+++ launcher/pybabel_update.sh
@@ -4,9 +4,9 @@
 pybabel extract -F babel.config -o lang/messages.pot .
 
 #pybabel update -l de_DE -d ./lang -i ./lang/messages.pot
-#pybabel update -l en_US -d ./lang -i ./lang/messages.pot
+pybabel update -l en_US -d ./lang -i ./lang/messages.pot
 #pybabel update -l es_VE -d ./lang -i ./lang/messages.pot
-#pybabel update -l fa_IR -d ./lang -i ./lang/messages.pot
+pybabel update -l fa_IR -d ./lang -i ./lang/messages.pot
 #pybabel update -l ja_JP -d ./lang -i ./lang/messages.pot
 pybabel update -l zh_CN -d ./lang -i ./lang/messages.pot
 
