dbaccf6d76da910f1a187d036ffbb8bc647690ad
Michael.X <xxnet.dev@gmail.com>
2016-04-09 18:31:27 +0800

    merge conflict

--- start.sh
+++ start.sh
@@ -2,10 +2,31 @@
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTPATH
+
 if python -V | grep -q "Python 3" ;then
-    PYTHON="/usr/bin/python2"
+    PYTHON="python2"
 else
     PYTHON="python"
 fi
 
-${PYTHON} launcher/start.py
+
+
+# launch xx-net service by ignore hungup signal
+function launchWithNoHungup() {
+    nohup ${PYTHON} launcher/start.py 2&> /dev/null &
+}
+
+# launch xx-net service by hungup signal
+function launchWithHungup() {
+    ${PYTHON} launcher/start.py
+}
+
+# get operating system name
+os_name=`uname -s`
+
+# Darwin for os x
+if [ $os_name = 'Darwin' ];then
+	launchWithNoHungup
+else
+	launchWithHungup
+fi
