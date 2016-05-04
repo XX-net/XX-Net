c9d05e38e8881e877ed9942241bf71e7611fa510
Michael.X <xxnet.dev@gmail.com>
2016-05-04 11:56:06 +0800

    remove cryptography 0.8.2

--- code/default/python27/1.0/lib/win32/cffi/__init__.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['FFI', 'VerificationError', 'VerificationMissing', 'CDefError',
-           'FFIError']
-
-from .api import FFI, CDefError, FFIError
-from .ffiplatform import VerificationError, VerificationMissing
-
-__version__ = "0.8.6"
-__version_info__ = (0, 8, 6)
