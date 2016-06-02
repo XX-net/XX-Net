985e2bcb5b51323f2d9988b96e2300e100ca6d48
yeahwu <wujiayu02@gmail.com>
2016-06-02 17:28:21 +0800

    fix can't update,XP SP3 can't start. #3534

--- code/default/python27/1.0/lib/noarch/socks.py
+++ code/default/python27/1.0/lib/noarch/socks.py
@@ -210,7 +210,7 @@ class socksocket(_BaseSocket):
     default_proxy = None
 
     def __init__(self, family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0, _sock=None):
-        if type != socket.SOCK_STREAM and type != socket.SOCK_DGRAM:
+        if type not in {socket.SOCK_STREAM, socket.SOCK_DGRAM}:
             msg = "Socket type must be stream or datagram, not {!r}"
             raise ValueError(msg.format(type))
 
