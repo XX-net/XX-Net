3a5d49166a34c2ea10c27a67ee7e8d0ccf3c9300
Michael.X <xxnet.dev@gmail.com>
2018-05-24 07:03:30 +0800

    fix bug:#10755, not include WinXP

--- code/default/gae_proxy/local/ipv6_tunnel/__init__.py
+++ code/default/gae_proxy/local/ipv6_tunnel/__init__.py
@@ -7,8 +7,7 @@ from .common import test_teredo
 
 if "arm" in platform.machine():
     from .unknown import state, enable, disable, set_best_server
-elif sys.platform == "win32":
-    # TODO: WinXP should be different
+elif sys.platform == "win32" and platform.release() != "XP":
     from .win10 import state, enable, disable, set_best_server
 elif sys.platform.startswith("linux"):
     from .linux import state, enable, disable, set_best_server
