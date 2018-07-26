582e7e2ed847c23e9a9b530b083c9eddd4365de8
YFdyh000 <yfdyh000@gmail.com>
2018-07-26 22:21:17 +0800

    Avoid creating version-dependent shortcut
    * Version directories may leave causing the shortcut to break
    * Icon path still depends

--- code/default/launcher/create_shortcut.js
+++ code/default/launcher/create_shortcut.js
@@ -1,9 +1,17 @@
+function getParent(path) {
+    var fso = new ActiveXObject("Scripting.FileSystemObject")
+    return fso.GetParentFolderName(path)
+}
+
 function CreateShortcut() {
-    wsh = new ActiveXObject('WScript.Shell');
-    fso = new ActiveXObject("Scripting.FileSystemObject")
+    var wsh = new ActiveXObject('WScript.Shell');
+    var fso = new ActiveXObject("Scripting.FileSystemObject");
     system_folder = fso.GetSpecialFolder(1)
     target_path = '"' + system_folder + '\\wscript.exe"';
-    argument_file = '"' + wsh.CurrentDirectory + '\\..\\..\\..\\start.vbs"';
+    xxnet_path = getParent(getParent(getParent(wsh.CurrentDirectory)));
+    //var shell = new ActiveXObject("WScript.Shell");
+    //shell.Popup(xxnet_path); // for debugging
+    argument_file = '"' + xxnet_path + '\\start.vbs"';
     icon_path = wsh.CurrentDirectory + '\\web_ui\\favicon.ico';
 
     link = wsh.CreateShortcut(wsh.SpecialFolders("Desktop") + '\\XX-Net.lnk');
@@ -12,7 +20,7 @@ function CreateShortcut() {
     link.WindowStyle = 7;
     link.IconLocation = icon_path;
     link.Description = 'XX-Net';
-    link.WorkingDirectory = wsh.CurrentDirectory;
+    link.WorkingDirectory = xxnet_path;
     link.Save();
 }
 
