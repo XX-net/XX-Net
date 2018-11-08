8bf5a8f4ea57a027022bae5644f41f4ddf4b879f
SeaHOH <Seahoh@Gmail.com>
2018-11-08 16:55:50 +0800

    bugfix

--- code/default/gae_proxy/local/proxy.py
+++ code/default/gae_proxy/local/proxy.py
@@ -120,7 +120,7 @@ def main(args):
     allow_remote = args.get("allow_remote", 0)
 
     listen_ips = front.config.listen_ip
-    if isinstance(listen_ips, str):
+    if isinstance(listen_ips, basestring):
         listen_ips = [listen_ips]
     else:
         listen_ips = list(listen_ips)
