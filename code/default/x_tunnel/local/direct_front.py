fbe530abd001c3b8b3eb4d60fe0d1c5be5a65c15
Michael.X <xxnet.dev@gmail.com>
2020-09-02 09:48:43 +1200

    Avoid high CPU when link fail.

--- code/default/x_tunnel/local/direct_front.py
+++ code/default/x_tunnel/local/direct_front.py
@@ -87,6 +87,7 @@ def request(method, host, schema="http", path="/", headers={}, data="", timeout=
         fail_num += 1
         continue_fail_num += 1
         last_fail_time = time.time()
+        time.sleep(1)
         return "", 602, {}
 
     last_success_time = time.time()
