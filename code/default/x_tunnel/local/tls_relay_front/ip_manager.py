2053ee8f8edd03c9ec955b49a72f53130c88e2f3
XX-Net.dev <xxnet.dev@gmail.com>
2022-10-18 19:09:04 -0400

    Fix bug.

--- code/default/x_tunnel/local/tls_relay_front/ip_manager.py
+++ code/default/x_tunnel/local/tls_relay_front/ip_manager.py
@@ -1,4 +1,3 @@
-import random
 
 from front_base.ip_manager import IpManagerBase
 
@@ -7,10 +6,25 @@ class IpManager(IpManagerBase):
     def __init__(self, config, host_manager, logger):
         super(IpManager, self).__init__(config, None, logger)
         self.host_manager = host_manager
+        self.ip_dict = {}
+        ips = self.host_manager.ips
+        self.ips = [ ip + ":443" for ip in ips]
 
-    def get_ip(self):
-        ip = random.choice(self.host_manager.ips)
-        return ip + ":443"
+    def report_connect_fail(self, ip_str, reason=""):
+        ip = ip_str.split(":")[0]
+        self.ip_dict.setdefault(ip, {
+            "fail_times": 0,
+            "success_times": 0
+        })
+        self.ip_dict[ip]["fail_times"] += 1
+
+    def update_ip(self, ip_str, handshake_time):
+        ip = ip_str.split(":")[0]
+        self.ip_dict.setdefault(ip, {
+            "fail_times": 0,
+            "success_times": 0
+        })
+        self.ip_dict[ip]["success_times"] += 1
 
     def recheck_ip(self, ip_str):
         pass
